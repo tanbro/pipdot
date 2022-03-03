257a8292ab43abced7aa76d17eaffecd2595815c
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-03 19:12:35 +0800

    optimize AddSysPath

--- src/pipdot/utils.py
+++ src/pipdot/utils.py
@@ -9,7 +9,4 @@ class AddSysPath():
         sys.path.insert(0, str(self._path))
 
     def __exit__(self, exc_type, exc_value, traceback):
-        try:
-            del sys.path[0]
-        except ValueError:
-            pass
+        del sys.path[0]
