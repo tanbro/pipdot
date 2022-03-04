e3599e6a454454680456edfa09578977179dd72d
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-04 15:18:59 +0800

    add: vendor freeze file

--- tools/make-vendor.sh
+++ tools/make-vendor.sh
@@ -4,6 +4,7 @@
     VENDOR_DIR=src/pipdot/_vendor 
     CIBUILDWHEEL=0
     python3 -m pip install --upgrade --no-compile -t ${VENDOR_DIR} -r requires/vendor.txt
+    python3 -m pip freeze  --path ${VENDOR_DIR} | tee ${VENDOR_DIR}/freeze.txt
     rm -rf ${VENDOR_DIR}/*.dist-info/ 
     rm -rf ${VENDOR_DIR}/markupsafe/_speedups.* 
 )
