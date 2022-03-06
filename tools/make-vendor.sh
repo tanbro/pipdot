4184e3bdf0007c998d0c81c7b0833d4658896e98
tanbro <tanbro@163.com>
2022-03-06 15:48:20 +0800

    adjust some scripts

--- tools/make-vendor.sh
+++ tools/make-vendor.sh
@@ -5,8 +5,8 @@ set -e
 ( 
     VENDOR_DIR=src/pipdot/_vendor 
     CIBUILDWHEEL=0
-    python -m pip install --upgrade --no-compile -t ${VENDOR_DIR} -r requires/vendor.txt
-    python -m pip freeze  --path ${VENDOR_DIR} | tee ${VENDOR_DIR}/freeze.txt
+    pip install --upgrade --no-compile -t ${VENDOR_DIR} -r requires/vendor.txt
+    pip freeze  --path ${VENDOR_DIR} | tee ${VENDOR_DIR}/freeze.txt
     rm -fr ${VENDOR_DIR}/*.dist-info/ 
     rm -fr ${VENDOR_DIR}/markupsafe/_speedups.* 
 )
