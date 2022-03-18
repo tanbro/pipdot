c2083f97411320368ecebd0fbec41b373f1e71a3
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-18 14:02:07 +0800

    editing pssh vencor script

--- scripts/update-vendor.ps1
+++ scripts/update-vendor.ps1
@@ -1,6 +1,6 @@
 $env:VENDOR_DIR = 'src/pipdot/_vendor'
 $env:CIBUILDWHEEL = '0'
 pip install -U --upgrade-strategy=eager --no-compile -t $env:VENDOR_DIR -r requires/vendor.txt
-pip freeze  --path $env:VENDOR_DIR | tee $env:VENDOR_DIR/freeze.txt
+pip freeze  --path $env:VENDOR_DIR | Tee-Object $env:VENDOR_DIR/freeze.txt
 Remove-Item -Force -Recurse $env:VENDOR_DIR/*.dist-info
 Remove-Item -Force -Recurse $env:VENDOR_DIR/markupsafe/_speedups.*
