760a692dfcd24dd65a9e13ce778275e97e69e1c5
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-18 11:45:23 +0800

    adjust dockerfile posiztion; update vendors

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:alpine
 
-COPY * /work/
+COPY dist/ /work/
 
 RUN    pip install --no-cache-dir --disable-pip-version-check pipdot --no-index --find-links file:///work/ \
     && rm -rf /work/
---
+++ scripts/update-vendor.ps1
@@ -0,0 +1,6 @@
+$env:VENDOR_DIR = 'src/pipdot/_vendor'
+$env:CIBUILDWHEEL = '0'
+pip install --upgrade --no-compile -t $env:VENDOR_DIR -r requires/vendor.txt
+pip freeze  --path $env:VENDOR_DIR | tee $env:VENDOR_DIR/freeze.txt
+Remove-Item -Force -Recurse $env:VENDOR_DIR/*.dist-info
+Remove-Item -Force -Recurse $env:VENDOR_DIR/markupsafe/_speedups.*
